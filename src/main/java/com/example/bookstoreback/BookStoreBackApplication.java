f705b70527b848a8c32d3c6a9e01a001cea38abe
Ihsan <ihsanelkabiri20004@gmail.com>
2023-03-30 11:34:32 +0000

    Initial commit

---
+++ src/main/java/com/example/bookstoreback/BookStoreBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookstoreback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreBackApplication.class, args);
+    }
+
+}
